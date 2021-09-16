bbceb171e5cdc10afde6014ba5081ac15ec8f878
LesleyJJ <jacobjohnlesley@gmail.com>
2021-09-16 16:29:07 +0530

    mongodb connector added

---
+++ connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// connect to mongodb
+mongoose.connect(process.env.MONGODB_URI,{}, (error) => {
+    if (error) {
+        console.log('unable to connect to the database!!');
+        console.log(error);
+        return;
+    }
+
+    console.log('connected to database');
+});
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
+require('./connection');
+
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.json("Hello from Hex Clan");
 });
 
-app.listen("8080", () => {
-  console.log("running on port 8080");
+app.listen(port, () => {
+  console.log("App is running on port: " + port);
 });
