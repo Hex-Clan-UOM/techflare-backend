f64c11cafd2f123a7113ed4445622e6c87d7b5ec
Thushitha Manokaran <manothushitha@gmail.com>
2021-09-18 12:03:27 +0530

    added another author, changed description

--- connection.js
+++ connection.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // connect to mongodb
-mongoose.connect(process.env.MONGODB_URI,{}, (error) => {
-    if (error) {
-        console.log('unable to connect to the database!!');
-        console.log(error);
-        return;
-    }
+mongoose.connect(process.env.MONGODB_URI, {}, (error) => {
+  if (error) {
+    console.log("unable to connect to the database!!");
+    console.log(error);
+    return;
+  }
 
-    console.log('connected to database: ' + process.env.MONGODB_URI);
+  console.log("connected to database");
 });
