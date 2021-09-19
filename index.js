b3550326f669eccefc6caa00be6bfee98022efb0
Thushitha Manokaran <manothushitha@gmail.com>
2021-09-19 17:24:36 +0530

    added body parsing

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require('./connection');
+require("./connection");
 const logger = require("./logger");
 
 const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.json("Hello from Hex Clan");
