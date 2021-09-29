d4e23ad38f57e8cffa8c4197b20fd55b606d136b
laroshan <laroshanlaro@gmail.com>
2021-09-29 21:11:05 +0530

    Debugging commit

--- appConfig.js
+++ appConfig.js
@@ -1,8 +1,6 @@
-module.exports = {
-  environment: process.env.NODE_ENV,
-  port: process.env.PORT,
-  mongoDB: process.env.MONGO_DB,
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  sessionSecret: process.env.SECRET,
-  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET
-};
+export const environment = process.env.NODE_ENV;
+export const port = process.env.PORT;
+export const mongoDB = process.env.MONGO_DB;
+export const googleClientId = process.env.GOOGLE_CLIENT_ID;
+export const sessionSecret = process.env.SECRET;
+export const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
