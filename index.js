8266a117201e707db167df7ca98788d6042a152e
LesleyJJ <jacobjohnlesley@gmail.com>
2021-10-02 09:37:42 +0530

    cors err on free flight req fixed

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const cors = require("cors");
 
 connectMongo();
 
-app.use(cors());
+const corsOptions ={
+   origin: appConfig.frontEnd, 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
